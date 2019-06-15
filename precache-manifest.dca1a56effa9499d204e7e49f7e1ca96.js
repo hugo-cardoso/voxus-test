234526641a44c52901f4bc429c3922f735251fac
Hugo Cardoso <hugorafaelmc@gmail.com>
2019-06-15 18:19:02 -0300

    Updates

---
+++ precache-manifest.dca1a56effa9499d204e7e49f7e1ca96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c07968add983e27920fe9e62c7c374",
+    "url": "/voxus-test/index.html"
+  },
+  {
+    "revision": "2d5fae900854c696c51b",
+    "url": "/voxus-test/static/js/2.5023d9ab.chunk.js"
+  },
+  {
+    "revision": "91fca08bc9dfde59a3ea",
+    "url": "/voxus-test/static/js/main.aabcab7a.chunk.js"
+  },
+  {
+    "revision": "2fed98b8af537c21dfe3",
+    "url": "/voxus-test/static/js/runtime~main.57383f05.js"
+  },
+  {
+    "revision": "d058f21d0138edb99d0835f7282b4e24",
+    "url": "/voxus-test/static/media/bg_Illustration.d058f21d.png"
+  },
+  {
+    "revision": "492bf844933201bd5a5363af21fe62cf",
+    "url": "/voxus-test/static/media/logo.492bf844.svg"
+  },
+  {
+    "revision": "c975c1d8c2c36141489d2379a6681766",
+    "url": "/voxus-test/static/media/logo_gray.c975c1d8.svg"
+  }
+]);
